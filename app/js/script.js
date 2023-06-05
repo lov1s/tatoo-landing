687635f9ed74e1c34374bd6c3b1e8c94a023dc39
lov1s <artem.boss619@gmail.com>
2023-06-05 16:35:41 +0300

    Smooth acardion

--- app/js/script.js
+++ app/js/script.js
@@ -17,10 +17,10 @@ for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("faq__active");
     let content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
     } else {
-      content.style.display = "block";
+      content.style.maxHeight = content.scrollHeight + "px";
     }
   });
 }
